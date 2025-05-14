40eae0052be9caf6aa5d5c2e35a0e40a3617207f
Ky Anh Nguyen <anh163.nka@gmail.com>
2025-05-14 17:50:11 +0700

    commit

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const jsonServer = require("json-server");
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
+
 // config
 const server = express();
-server.use(cors());
-server.use(express.static(path.join(__dirname, "./public", "../public")));
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
 
-// setting up EJS
-server.set("view engine", "ejs");
-server.set("views", path.join(__dirname, "./views", "../views"));
-
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use("/", router);
+server.use("/api", middlewares, router);
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
